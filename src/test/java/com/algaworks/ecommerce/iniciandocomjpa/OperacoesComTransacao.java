b55d1182e7ba6982cc488641c90861f190c8967c
Fabio Ferreira <fabiofs.br@gmail.com>
2023-08-10 12:33:51 -0300

    2.8. Abrindo e fechando uma transação

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacao.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.jupiter.api.Test;
+
+public class OperacoesComTransacao extends EntityManagerTest {
+
+    @Test
+    public void abrirEFecharATransacao() {
+//        Produto produto = new Produto(); // Somente para o método não mostrar erros
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.persist(produto);
+//        entityManager.merge(produto);
+//        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+
+    }
+
+}
