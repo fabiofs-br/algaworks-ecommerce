ec0c7b2781dba666f5113037aab6a200d3fb10ac
Fabio Ferreira <fabiofs.br@gmail.com>
2023-08-30 11:44:27 -0300

    6.9. Mapeando coleções de tipos básicos com @ElementCollection

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.mapeamentoavancado;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ElementCollectionTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarTags() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setTags(Arrays.asList("ebook", "livro-digital"));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+
+        Assertions.assertFalse(produtoVerificacao.getTags().isEmpty());
+    }
+
+
+}
