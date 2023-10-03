7caab6d8e76f04f0618f190b0447f127e1914ad5
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-03 17:21:53 -0300

    8.5. Exercício: fazendo atualizações em cascata

--- src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
+++ src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.operacoesemcascata;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.ItemPedidoId;
@@ -10,9 +11,36 @@ import com.algaworks.ecommerce.model.StatusPedido;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class CascadeTypeMergeTest extends EntityManagerTest {
+
+//    @Test
+    public void atualizarProdutoComCategoria() {
+        Produto produto = new Produto();
+        produto.setId(1);
+        produto.setDataUltimaAtualizacao(LocalDateTime.now());
+        produto.setPreco(new BigDecimal(500));
+        produto.setNome("Kindle");
+        produto.setDescricao("Agora com iluminação embutida ajustavel.");
+
+        Categoria categoria = new Categoria();
+        categoria.setId(2);
+        categoria.setNome("Tablets");
+
+        produto.setCategorias(Arrays.asList(categoria)); // CascadeType.MERGE
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assertions.assertEquals("Tablets", categoriaVerificacao.getNome());
+    }
 
     //    @Test
     public void atualizarPedidoComItens() {
