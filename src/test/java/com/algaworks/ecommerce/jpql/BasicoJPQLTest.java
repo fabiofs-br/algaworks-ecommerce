a5721d3d8c109e447fb360263377a5d605a20410
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-06 15:47:32 -0300

    9.1. Introdução à JPQL (Java Persistence Query Language)

---
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -0,0 +1,30 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPorIdentificador() {
+        // entityManager.find(Pedido.class, 1)
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+
+        // select p.* from pedido p where p.id = 1
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assertions.assertNotNull(pedido);
+
+//        List<Pedido> lista = typedQuery.getResultList();
+//        Assertions.assertFalse(lista.isEmpty());
+
+    }
+
+}
