497958ace82f25f3f3b6730857d09e8d147143ec
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-09 15:49:48 -0300

    9.6. Fazendo inner join entre as entidades

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -7,10 +7,11 @@ INSERT INTO cliente (id, nome, cpf) VALUES (2, 'Marcos Mariano', '111');
 INSERT INTO cliente_detalhe (cliente_id, sexo, data_nascimento) VALUES (1, 'MASCULINO', DATE_SUB(SYSDATE(), INTERVAL 27 YEAR));
 INSERT INTO cliente_detalhe (cliente_id, sexo, data_nascimento) VALUES (2, 'MASCULINO', DATE_SUB(SYSDATE(), INTERVAL 30 YEAR));
 
-INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (1, 1, SYSDATE(), 998.0, 'AGUARDANDO');
+INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (1, 1, DATE_SUB(SYSDATE(), INTERVAL 5 DAY), 2398.0, 'AGUARDANDO');
 INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (2, 1, SYSDATE(), 499.0, 'AGUARDANDO');
 
 INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (1, 1, 499, 2);
+INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (1, 3, 1400, 1);
 INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (2, 1, 499, 1);
 
 INSERT INTO pagamento (pedido_id, status, numero_cartao, tipo_pagamento) VALUE (2, 'PROCESSANDO', '123', 'cartao');
---
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        String jpql = "select p from Pedido p join p.pagamento pag";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 1);
+    }
+
+}
