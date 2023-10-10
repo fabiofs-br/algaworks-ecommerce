0866308f39c672823270c28ccd15d6c795d2b47f
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-10 14:42:11 -0300

    9.22. Usando funções para strings

---
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesStringsTest.java
@@ -0,0 +1,26 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FuncoesStringsTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncao() {
+        // concat, length, locate, substring, lower, upper, trim
+
+        String jpql = "select c.nome, length(c.nome) from Categoria c where substring(c.nome, 1, 1) = 'N' ";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
+    }
+
+}
