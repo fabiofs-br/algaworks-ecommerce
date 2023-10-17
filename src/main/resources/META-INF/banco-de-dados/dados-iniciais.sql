200c1b792c012099941f65558395c90a31a6a2df
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-17 10:09:39 -0300

    9.28. Agrupando o registros com group by

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,5 +1,6 @@
 INSERT INTO produto (id, nome, preco, data_criacao, descricao) VALUES (1, 'Kindle', 499.0, DATE_SUB(SYSDATE(), INTERVAL 1 DAY), 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 INSERT INTO produto (id, nome, preco, data_criacao, descricao) VALUES (3, 'Câmera GoPro Hero 7', 1400.0, DATE_SUB(SYSDATE(), INTERVAL 1 DAY), 'Desempenho 2x melhor.');
+INSERT INTO produto (id, nome, preco, data_criacao, descricao) VALUES (4, 'Câmera Canon 80D', 3500.0, SYSDATE(), 'O melhor ajuste de foco.');
 
 INSERT INTO cliente (id, nome, cpf) VALUES (1, 'Fernando Medeiros', '000');
 INSERT INTO cliente (id, nome, cpf) VALUES (2, 'Marcos Mariano', '111');
@@ -7,25 +8,30 @@ INSERT INTO cliente (id, nome, cpf) VALUES (2, 'Marcos Mariano', '111');
 INSERT INTO cliente_detalhe (cliente_id, sexo, data_nascimento) VALUES (1, 'MASCULINO', DATE_SUB(SYSDATE(), INTERVAL 27 YEAR));
 INSERT INTO cliente_detalhe (cliente_id, sexo, data_nascimento) VALUES (2, 'MASCULINO', DATE_SUB(SYSDATE(), INTERVAL 30 YEAR));
 
-INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (1, 1, DATE_SUB(SYSDATE(), INTERVAL 5 DAY), 2398.0, 'AGUARDANDO');
-INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (2, 1, SYSDATE(), 499.0, 'AGUARDANDO');
+INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (1, 1, DATE_SUB(SYSDATE(), INTERVAL 32 DAY), 2398.0, 'AGUARDANDO');
+INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (2, 1, DATE_SUB(SYSDATE(), INTERVAL 5 DAY), 499.0, 'AGUARDANDO');
+INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (3, 1, DATE_SUB(SYSDATE(), INTERVAL 4 DAY), 3500.0, 'PAGO');
+INSERT INTO pedido (id, cliente_id, data_criacao, total, status) VALUES (4, 2, DATE_SUB(SYSDATE(), INTERVAL 2 DAY), 499.0, 'PAGO');
 
 INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (1, 1, 499, 2);
 INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (1, 3, 1400, 1);
 INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (2, 1, 499, 1);
+INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (3, 4, 3500, 1);
+INSERT INTO item_pedido (pedido_id, produto_id, preco_produto, quantidade) VALUES (4, 1, 499, 1);
 
-INSERT INTO pagamento (pedido_id, status, numero_cartao, tipo_pagamento) VALUE (2, 'PROCESSANDO', '123', 'cartao');
+INSERT INTO pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) VALUES (2, 'PROCESSANDO', 'cartao', '123', NULL);
 
-INSERT INTO nota_fiscal (pedido_id, xml, data_emissao) values (2, '<xml />', SYSDATE());
+INSERT INTO nota_fiscal (pedido_id, xml, data_emissao) VALUES (2, '<xml />', SYSDATE());
 
-INSERT INTO categoria (nome) VALUES ('Eletrodomésticos');
-INSERT INTO categoria (nome) VALUES ('Livros');
-INSERT INTO categoria (nome) VALUES ('Esportes');
-INSERT INTO categoria (nome) VALUES ('Futebol');
-INSERT INTO categoria (nome) VALUES ('Natação');
-INSERT INTO categoria (nome) VALUES ('Notebooks');
-INSERT INTO categoria (nome) VALUES ('Smartphones');
+INSERT INTO categoria (id, nome) VALUES (1, 'Eletrodomésticos');
+INSERT INTO categoria (id, nome) VALUES (2, 'Livros');
+INSERT INTO categoria (id, nome) VALUES (3, 'Esportes');
+INSERT INTO categoria (id, nome) VALUES (4, 'Futebol');
+INSERT INTO categoria (id, nome) VALUES (5, 'Natação');
+INSERT INTO categoria (id, nome) VALUES (6, 'Notebooks');
+INSERT INTO categoria (id, nome) VALUES (7, 'Smartphones');
+INSERT INTO categoria (id, nome) VALUES (8, 'Câmeras');
 
 INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (1, 2);
-
-
+INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (3, 8);
+INSERT INTO produto_categoria (produto_id, categoria_id) VALUES (4, 8);
---
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -0,0 +1,36 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class GroupByTest extends EntityManagerTest {
+
+    @Test
+    public void agruparResultado() {
+//        Quantidade de produtos por categoria.
+//        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+
+//        Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao)";
+
+//        Total de vendas por categoria.
+        String jpql = "select c.nome, sum(ip.precoProduto * ip.quantidade) from ItemPedido ip " +
+                " join ip.produto pro join pro.categorias c " +
+                " group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+}
