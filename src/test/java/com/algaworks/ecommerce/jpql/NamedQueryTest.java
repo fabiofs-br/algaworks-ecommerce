f79d0b386c3a197d641aea2ce8ec66c6844c4417
Fabio Ferreira <fabiofs.br@gmail.com>
2023-10-23 18:13:03 -0300

    9.48. Configurando uma query nomeada com @NamedQuery

---
+++ src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executarConsulta() {
+        TypedQuery<Produto> typedQuery = entityManager.createNamedQuery("Produto.listarPorCategoria", Produto.class);
+        typedQuery.setParameter("categoria", 2);
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+}
