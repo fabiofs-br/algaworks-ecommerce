2bac1a6f3a223e1ca4ba44a2ebd0fbc2758ffcbf
Fabio Ferreira <fabiofs.br@gmail.com>
2023-11-07 19:05:16 -0300

    10.7. Fazendo inner join entre as entidades

---
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,39 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.ItemPedido;
+import com.algaworks.ecommerce.model.Pagamento;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        Join<Pedido, ItemPedido> joinItens = root.join("itens");
+        Join<ItemPedido, Produto> joinItemProduto = joinItens.join("produto");
+
+        criteriaQuery.select(root);
+
+//        criteriaQuery.select(joinPagamento);
+//        criteriaQuery.where(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 4);
+    }
+
+}
