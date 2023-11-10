a5c8884c8ff39aafa9cfcb8d8cd41d277e7ca92f
Fabio Ferreira <fabiofs.br@gmail.com>
2023-11-10 17:05:45 -0300

    10.18. Usando expressão condicional between

--- src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -21,6 +21,27 @@ import java.util.List;
 public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void usarBetween() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.between(root.get(Pedido_.dataCriacao),
+                        LocalDateTime.now().minusDays(5).withSecond(0).withMinute(0).withHour(0),
+                        LocalDateTime.now())
+        );
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(p -> System.out.println("ID: " + p.getId() + ", Total: " + p.getTotal()));
+    }
+
+    @Test
     public void usarMaiorMenorComDatas() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
