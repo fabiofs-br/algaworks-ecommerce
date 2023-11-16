295f779027bab95a9af907850519155709d4db95
Fabio Ferreira <fabiofs.br@gmail.com>
2023-11-16 14:36:30 -0300

    10.28. Usando funções de agregação

--- src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -22,6 +22,34 @@ import java.util.List;
 public class FuncoesCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoAgragacao() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                criteriaBuilder.count(root.get(Pedido_.id)),
+                criteriaBuilder.avg(root.get(Pedido_.total)),
+                criteriaBuilder.sum(root.get(Pedido_.total)),
+                criteriaBuilder.min(root.get(Pedido_.total)),
+                criteriaBuilder.max(root.get(Pedido_.total))
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                "count: " + arr[0]
+                        + ", avg: " + arr[1]
+                        + ", sum: " + arr[2]
+                        + ", min: " + arr[3]
+                        + ", max: " + arr[4]
+        ));
+    }
+
+    @Test
     public void aplicarFuncaoNativa() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -43,7 +71,6 @@ public class FuncoesCriteriaTest extends EntityManagerTest {
         lista.forEach(arr -> System.out.println(
                 arr[0] + ", dayname: " + arr[1]
         ));
-
     }
 
     @Test
