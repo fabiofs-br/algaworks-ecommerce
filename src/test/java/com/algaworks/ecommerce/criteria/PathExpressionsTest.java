b8890b45338a9801173b30a37f9eaa179bb072e1
Fabio Ferreira <fabiofs.br@gmail.com>
2023-11-16 17:23:14 -0300

    10.32. Exercício: consultando pedidos com produto específico

--- src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/PathExpressionsTest.java
@@ -2,11 +2,15 @@ package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente_;
+import com.algaworks.ecommerce.model.ItemPedido;
+import com.algaworks.ecommerce.model.ItemPedido_;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Pedido_;
+import com.algaworks.ecommerce.model.Produto_;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,6 +18,24 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPedidosComProdutoDeIdIgual1Exercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> joinPedidoItemPedido = root.join(Pedido_.itens);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.equal(joinPedidoItemPedido.get(ItemPedido_.produto).get(Produto_.id), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void usarPathExpression() {
