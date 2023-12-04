86013e027efd3ab859083a961b6a981646cd2383
Fabio Ferreira <fabiofs.br@gmail.com>
2023-12-04 17:28:12 -0300

    11.15. Invocando uma view do banco de dados

--- src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.consultasnativas;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import jakarta.persistence.ParameterMode;
+import jakarta.persistence.Query;
 import jakarta.persistence.StoredProcedureQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,30 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class StoredProceduresTest extends EntityManagerTest {
+
+    @Test
+    public void executeView() {
+        Query query = entityManager.createNativeQuery(
+                "select cli.id, cli.nome, sum(ped.total) " +
+                        " from pedido ped " +
+                        " join view_clientes_acima_media cli on cli.id = ped.cliente_id " +
+                        " group by ped.cliente_id");
+
+        List<Object[]> lista = query.getResultList();
+
+        lista.stream().forEach(arr -> System.out.println(String.format("Cliente => ID: %s, Nome: %s, Total: %s", arr)));
+    }
+
+    @Test
+    public void executeViewRetornandoCliente() {
+        Query query = entityManager.createNativeQuery(
+                "select * from view_clientes_acima_media", Cliente.class);
+
+        List<Cliente> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Cliente => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
 
     @Test
     public void chamarNamedStoreProcedure() {
